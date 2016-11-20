685f09a447c95663eb2236f958c5e023a2471bd1
Scarlett <gourleys@tcd.ie>
2016-11-20 19:13:08 +0000

    Cleaned up potential namespacing issues

--- include/snake.h
+++ include/snake.h
@@ -1,16 +1,25 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
-struct Snake {
+struct Coord {
 	int x, y;
+};
+
+struct Snake {
+	struct Coord* dir;
 	int length;
 };
+
+struct Coord* new_coord();
+void delete_coord(struct Coord* coord);
 
 struct Snake* new_snake();
 void delete_snake(struct Snake* snake);
 void move_snake(struct Snake* snake, int dirx, int diry);
+void update_snake_length(struct Snake* snake);
 
-int get_x(struct Snake* snake);
-int get_y(struct Snake* snake);
+int get_snake_x(struct Snake* snake);
+int get_snake_y(struct Snake* snake);
+int get_snake_len(struct Snake* snake);
 
 #endif
--- src/snake.c
+++ src/snake.c
@@ -1,27 +1,46 @@
 #include "snake.h"
 #include <stdlib.h>
 
+struct Coord* new_coord(int x, int y) {
+	struct Coord* coord = malloc(sizeof(struct Coord));
+	coord->x = x;
+	coord->y = y;
+	return coord;
+}
+
+void delete_coord(struct Coord* coord) {
+	free(coord);
+}
+
 struct Snake* new_snake() {
 	struct Snake* snake = malloc(sizeof(struct Snake));
 	snake->length = 1;
-	snake->x = 0;
-	snake->y = 0;
+	snake->dir = new_coord(0, 0);
 	return snake;
 }
 
 void delete_snake(struct Snake* snake) {
+	delete_coord(snake->dir);
 	free(snake);
 }
 
 void move_snake(struct Snake* snake, int dirx, int diry) {
-	snake->x = dirx;
-	snake->y = diry;
+	snake->dir->x = dirx;
+	snake->dir->y = diry;
 }
 
-int get_x(struct Snake* snake) {
-	return snake->x;
+void update_snake_length(struct Snake* snake) {
+	snake->length++;
 }
 
-int get_y(struct Snake* snake) {
-	return snake->y;
+int get_snake_x(struct Snake* snake) {
+	return snake->dir->x;
+}
+
+int get_snake_y(struct Snake* snake) {
+	return snake->dir->y;
+}
+
+int get_snake_len(struct Snake* snake) {
+	return snake->length;
 }
